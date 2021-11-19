bd5ee55fed2526b863020f00b6b730142c7d33ff
Jakub Kákona <kaklik@mlab.cz>
2021-11-19 22:19:19 +0100

    Improve texts and images.

---
+++ doc/RPD/src/img/merge.sh
@@ -0,0 +1,4 @@
+
+#https://stackoverflow.com/questions/20737061/merge-images-side-by-sidehorizontally
+
+montage FIK-6_acceleration.png FIK-6_angular_speed.png -tile 1x2 -geometry +0+0 FIK-6_metadada.png
