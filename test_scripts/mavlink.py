5a9f3e9b89cda484ed1f7df1b4a3f062d92ca0c5
slimonslimon <vithanousek@seznam.cz>
2023-11-20 17:07:14 +0100

    Update mavlink.py
    update to lasted tested version. (work on Sik modems or USB, not on wifilink)

--- test_scripts/mavlink.py
+++ test_scripts/mavlink.py
@@ -9,15 +9,14 @@
 ##spustit
 ##MAVLINK20=1 python3 mavlink.py 
 
-
 from pymavlink.dialects.v20 import common as mavlink2
 from pymavlink import mavutil
 import time
 
-the_connection = mavutil.mavlink_connection('/dev/ttyS0',baud=9600)
+the_connection = mavutil.mavlink_connection('/dev/ttyS0',baud=9600,source_system=1,source_component=138)
 
 the_connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
-                                                mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
+                                              mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
 
 the_connection.wait_heartbeat()
 
@@ -30,5 +29,8 @@ while True:
 
   #
   payload=bytearray(128)
-  the_connection.mav.tunnel_send(target_system=255,target_component=0,payload_type=305,payload_length=1,payload=payload)
-  time.sleep(2)
+  the_connection.mav.tunnel_send(target_system=0,target_component=0,payload_type=305,payload_length=1,payload=payload)
+  #the_connection.mav.debug_send(time_boot_ms=15,ind=5,value=0.15)
+  
+  the_connection.wait_heartbeat()
+  print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
